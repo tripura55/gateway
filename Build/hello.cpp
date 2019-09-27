35b062858b971ff09cbe31cb066b10542e1c9ca0
tripura55 <54296696+tripura55@users.noreply.github.com>
2019-09-27 13:07:14 +0530

    Update hello.cpp

--- Build/hello.cpp
+++ Build/hello.cpp
@@ -5,7 +5,7 @@ int main(void)
 {
 
    printf ("Hello from your first program!\n");
-   printf ("Hello everyone!\n");
+   printf ("Hello !\n");
    printf (" s !\n");
 
    printf("Hello world!\n");
